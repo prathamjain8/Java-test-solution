a19ff567a661e91acc6cb4856d70909a491f8fec
Pratham Jain <jainpj82003@gmail.com>
2024-04-25 13:44:27 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,3 +5,24 @@ function longestSubstring(s) {
 module.exports = { longestSubstring };
 
 
+
+function longestSubstring(s) {
+    let maxLength = 0;
+    let start = 0;
+    let charIndexMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (charIndexMap.hasOwnProperty(s[i]) && charIndexMap[s[i]] >= start) {
+            start = charIndexMap[s[i]] + 1;
+        }
+        charIndexMap[s[i]] = i;
+        maxLength = Math.max(maxLength, i - start + 1);
+    }
+
+    return maxLength;
+}
+
+module.exports = { longestSubstring };
+
+// Example usage:
+// console.log(longestSubstring("abcabcbb")); // Output: 3
